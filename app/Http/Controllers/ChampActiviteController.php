07c37a0fc8a820b584912e5c8beac3fe10dd1f27
kamatcho17 <kamelraw@hotmail.fr>
2018-04-09 17:38:50 +0200

    CRUD score + prospect + champActivité Version 1

--- app/Http/Controllers/ChampActiviteController.php
+++ app/Http/Controllers/ChampActiviteController.php
@@ -4,7 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\ChampActivite;
+
 class ChampActiviteController extends Controller
 {
-    //
+    public function store(){
+
+    	$this->validate(request(),[
+    		'LibChampAct'->'required'
+    	]);
+
+
+		Score::create([
+    			'LibChampAct' => request('LibChampAct')
+		]);
+ 
+		redirect('/');
+    }
+
+    public function update(ChampActivite $champActivite){
+
+    	$this->validate(request(),[
+    		'LibChampAct'->'required'
+    	]);
+
+    	$champActivite->LibChampAct => request('LibChampAct');
+
+    
+    	$score->save();
+    }
+
+    public function destroy(ChampActivite $champActivite){
+
+    	$champActivite->delete();
+    }
 }
--- app/Http/Controllers/ProspectController.php
+++ app/Http/Controllers/ProspectController.php
@@ -4,7 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Prospect;
+
 class ProspectController extends Controller
 {
-    //
+    public function store(){
+
+    	$this->validate(request(),[
+    		'codeProsp'->'required'
+    		'idChampAct'->'required'
+    		'societe'->'required'
+    		'wilaya'->'required'
+    		'tele1'->'required'
+    	]);
+
+
+		Score::create(request()->all());
+ 
+		redirect('/');
+    }
+
+    public function update(Prospect $prospect){
+    	$this->validate(request(),[
+    		'LibScore'->'required'
+    	]);
+    		/*
+    			Ndirouhoum ga3 un par un wella kayan plus simple ??
+			*/
+
+    	$score->save();
+    }
+
+    public function destroy(Prospect $prospect){
+
+    	$prospect->delete();
+    }
 }
